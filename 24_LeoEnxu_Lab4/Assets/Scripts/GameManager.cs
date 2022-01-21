65551cc03282f0f665c54ee0afd3acdc0a17dcda
24-LeoEnxu <leoenxu@gmail.com>
2022-01-21 15:03:47 +0800

    Game Created
    Level 1 Added, Win, Lose Conditions Added, Prefebs Set

---
+++ 24_LeoEnxu_Lab4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Text coinsLeft;
+    public static int coins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinsLeft.text = "Coins Left: " + coins;
+
+        if (coins <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ 24_LeoEnxu_Lab4/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovementScript : MonoBehaviour
+{
+    public float speed;
+    Rigidbody playerRb;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float hMovement = Input.GetAxis("Horizontal");
+        float vMovement = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(hMovement, 0, vMovement);
+        playerRb.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            GameManager.coins -= 1;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Hazard"))
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+}
